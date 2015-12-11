1e8fd9fa0ba2924c3870a9d684b1da4ac0206d70
varurupeta <varurupeta@gmail.com>
2015-12-11 13:03:43 -0800

    intial commit

---
+++ src/main/java/org/mani/java/App.java
@@ -0,0 +1,15 @@
+package org.mani.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello JAVA World!" );
+        System.out.println( "this is vesion-1.1" );
+     
+    }
+}
